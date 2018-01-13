84756ef3d072e4668deb20445aed57b8f00d5bcf
Matt Ross <amsross@gmail.com>
2018-01-13 16:38:18 -0500

    chore: simple style

--- lib.js
+++ lib.js
@@ -4,6 +4,51 @@ const { DateTime, Duration } = require('luxon')
 const { compose, invoke, map, nth, toPairs } = require('./helpers')
 const intervals = require('./intervals')
 
+const prefix = css`
+  :host { }
+  :host .units {
+    color: #CCC;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  :host .unit {
+    border: 1px solid #ccc;
+    padding: 0px 5px;
+    border-right: 0;
+    border-bottom: 0;
+    margin-top: 5px;
+    margin-right: 5px;
+    margin-bottom: 1px;
+  }
+  :host .unit.on {
+    border-color: #DF928E;
+    color: #DF928E;
+  }
+
+  :host .week { }
+  :host .day { }
+  :host .hour { }
+  :host .minute { }
+
+  :host > .weeks { }
+  :host > .weeks .week { }
+  :host > .weeks .day { }
+  :host > .weeks .hour { }
+  :host > .weeks .minute { }
+
+  :host > .days { }
+  :host > .days .day { }
+  :host > .days .hour { }
+  :host > .days .minute { }
+
+  :host > .hours { }
+  :host > .hours .hour { }
+  :host > .hours .minute { }
+
+  :host > .minutes { }
+  :host > .minutes .minute { }
+`
+
 const visualize = state => {
   const now = DateTime.local()
   const dur = Duration.fromObject({ seconds: state.repeats * state.interval })
@@ -13,47 +58,6 @@ const visualize = state => {
     nth(0),
     toPairs,
     invoke('toObject'))(end.diff(now, ['minutes', 'hours', 'days', 'weeks']))
-
-  const prefix = css`
-    :host { }
-    :host .units {
-      color: #CCC;
-      display: flex;
-      flex-wrap: wrap;
-    }
-    :host .unit {
-      border: 1px solid #CCC;
-      padding: 5px;
-      margin-bottom: -1px;
-    }
-    :host .unit.on {
-      border-color: #DF928E;
-      color: #DF928E;
-    }
-
-    :host .week { }
-    :host .day { }
-    :host .hour { }
-    :host .minute { }
-
-    :host > .weeks { }
-    :host > .weeks .week { }
-    :host > .weeks .day { }
-    :host > .weeks .hour { }
-    :host > .weeks .minute { }
-
-    :host > .days { }
-    :host > .days .day { }
-    :host > .days .hour { }
-    :host > .days .minute { }
-
-    :host > .hours { }
-    :host > .hours .hour { }
-    :host > .hours .minute { }
-
-    :host > .minutes { }
-    :host > .minutes .minute { }
-  `
 
   return html`
     <div>
