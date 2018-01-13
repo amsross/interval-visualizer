16ba1652fbc9d2b493e882a6970b6fee35881c8e
Matt Ross <amsross@gmail.com>
2018-01-13 13:01:24 -0500

    chore: Remove exploratory flex layout

--- lib.js
+++ lib.js
@@ -15,64 +15,44 @@ const visualize = state => {
     invoke('toObject'))(end.diff(now, ['minutes', 'hours', 'days', 'weeks']))
 
   const prefix = css`
-    :host {
-      min-width: 600px;
-      height: 100%;
-      display: flex;
-      justify-content: space-between;
-      align-content: space-between;
-    }
+    :host { }
     :host .units {
-      color: black;
+      color: #CCC;
       display: flex;
       flex-wrap: wrap;
-      justify-content: space-between;
-      align-content: space-between;
     }
-    :host .units .unit {
-      border: 1px solid black;
+    :host .unit {
+      border: 1px solid #CCC;
       padding: 5px;
-      background-color: #FFF;
+      margin-bottom: -1px;
     }
-    :host .units .unit.on {
-      border-color: #CCC;
-      background-color: #CCC;
-      color: #FFF !important;
+    :host .unit.on {
+      border-color: #DF928E;
+      color: #DF928E;
     }
-    :host .units .unit.week {
-      margin: 20px auto;
-    }
-    :host .units .unit.week.on {
-      background-color: #DF928E;
-    }
-    :host > .units.days {
-      width: 100%;
-    }
-    :host > .units.days .day {
-      flex-basis: 50%;
-      flex-grow: 0;
-    }
-    :host > .units.days .hours {
-    }
-    :host > .units.days .hour {
-      flex-basis: 15%;
-      flex-grow: 1;
-    }
-    :host .units .unit.day.on {
-      background-color: #C58882;
-    }
-    :host .units .unit.hour.on {
-      background-color: #415A77;
-    }
-    :host .units .unit.minute.on {
-      background-color: #208AAE;
-    }
-    :host .units.minutes .minute:nth-of-type(10) {
-      align-self: flex-end;
-    }
-    :host .units .unit .unit {
-      color: black;
-    }
+
+    :host .week { }
+    :host .day { }
+    :host .hour { }
+    :host .minute { }
+
+    :host > .weeks { }
+    :host > .weeks .week { }
+    :host > .weeks .day { }
+    :host > .weeks .hour { }
+    :host > .weeks .minute { }
+
+    :host > .days { }
+    :host > .days .day { }
+    :host > .days .hour { }
+    :host > .days .minute { }
+
+    :host > .hours { }
+    :host > .hours .hour { }
+    :host > .hours .minute { }
+
+    :host > .minutes { }
+    :host > .minutes .minute { }
   `
 
   return html`
