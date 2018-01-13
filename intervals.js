3c486912ed6eca54670524a70280dd914b321b16
Matt Ross <amsross@gmail.com>
2018-01-13 13:08:43 -0500

    chore: template logic cleanup steps

--- intervals.js
+++ intervals.js
@@ -2,11 +2,13 @@ const html = require('choo/html')
 const { mathMod, max } = require('./helpers')
 
 const weeks = (iterations, first, interval, depth) => {
+  const count = max(Math.ceil(iterations - first), iterations)
+  const isOn = i => mathMod(first + i, Math.floor(interval / 60 / 60 / 24 / 7) || 1) === 0
+
   return html`
     <div class="units weeks">
-      ${Array(max(Math.ceil(iterations - first), iterations)).fill(null).map((x, i) => html`
-        <div class="unit week
-            ${mathMod(first + i, Math.floor(interval / 60 / 60 / 24 / 7) || 1) === 0 ? 'of' : 'off'}">
+      ${Array(count).fill(null).map((x, i) => html`
+        <div class="unit week ${isOn(i) ? 'on' : 'off'}">
           ${(depth < 1) ? i + 1 : days(7,
             mathMod(Math.floor((interval / 60 / 60 / 24) - (i * 60)) || 1, Math.floor(interval / 60 / 60 / 24) || 1),
             interval,
@@ -18,11 +20,13 @@ const weeks = (iterations, first, interval, depth) => {
 }
 
 const days = (iterations, first, interval, depth) => {
+  const count = max(Math.ceil(iterations - first), iterations)
+  const isOn = i => mathMod(first + i, Math.floor(interval / 60 / 60 / 24) || 1) === 0
+
   return html`
     <div class="units days">
-      ${Array(max(Math.ceil(iterations - first), iterations)).fill(null).map((x, i) => html`
-        <div class="unit day
-            ${mathMod(first + i, Math.floor(interval / 60 / 60 / 24) || 1) === 0 ? 'on' : 'off'}">
+      ${Array(count).fill(null).map((x, i) => html`
+        <div class="unit day ${isOn(i) ? 'on' : 'off'}">
           ${(depth < 1) ? (i + 1).toString().padStart(2, 0) : hours(24,
             mathMod(Math.floor((interval / 60 / 60) - (i * 60)) || 1, Math.floor(interval / 60 / 60) || 1),
             interval,
@@ -34,11 +38,13 @@ const days = (iterations, first, interval, depth) => {
 }
 
 const hours = (iterations, first, interval, depth) => {
+  const count = max(Math.ceil(iterations - first), iterations)
+  const isOn = i => mathMod(first + i, Math.floor(interval / 60 / 60) || 1) === 0
+
   return html`
     <div class="units hours">
-      ${Array(max(Math.ceil(iterations - first), iterations)).fill(null).map((x, i) => html`
-        <div class="unit hour
-            ${mathMod(first + i, Math.floor(interval / 60 / 60) || 1) === 0 ? 'on' : 'off'}">
+      ${Array(count).fill(null).map((x, i) => html`
+        <div class="unit hour ${isOn(i) ? 'on' : 'off'}">
           ${(depth < 1) ? (i + 1).toString().padStart(2, 0) : minutes(60,
             mathMod(Math.floor((interval / 60) - (i * 60)) || 1, Math.floor(interval / 60) || 1),
             interval,
@@ -50,11 +56,13 @@ const hours = (iterations, first, interval, depth) => {
 }
 
 const minutes = (iterations, first, interval, depth) => {
+  const count = max(Math.ceil(iterations - first), iterations)
+  const isOn = i => mathMod(first + i, Math.floor(interval / 60) || 1) === 0
+
   return html`
     <div class="units minutes db normal">
-      ${Array(max(Math.ceil(iterations - first), iterations)).fill(null).map((x, i) => html`
-        <div class="unit minute
-            ${mathMod(first + i, Math.floor(interval / 60) || 1) === 0 ? 'on' : 'off'}">
+      ${Array(count).fill(null).map((x, i) => html`
+        <div class="unit minute ${isOn(i) ? 'on' : 'off'}">
             ${(i + 1).toString().padStart(2, 0)}
         </div>
       `)}
